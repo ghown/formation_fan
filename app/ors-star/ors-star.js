3da12731e6d2fe9671e94f4a27f86b3757a5cbd5
Maxime PIRES <maxime.pires@outlook.com>
2016-06-21 08:22:36 +0100

    J'ai ajouté des motherfucking étoiles

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -6,8 +6,16 @@
 	app.directive('orsStar', function() {
 		return {
 			restrict: 'AEC',
-			link: function() {
-			console.log('orsStar', arguments);
+			link: function(scope, element, attrs) {
+				console.log('orsStar', arguments);
+				var html = '';
+				for (var i = 0; i < 2; i++){
+					html += '<img src="ors-star/img/yellow_star.png" />';
+				}
+				for (var i = 0; i < 3; i++){
+					html += '<img src="ors-star/img/white_star.png" />';
+				}
+				element.append(html);
 			}
 		};
 	});
