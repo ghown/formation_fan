1cea766d6f073c170a479cb4920ebe683e6a7d61
Maxime PIRES <maxime.pires@outlook.com>
2016-06-21 08:29:40 +0100

    Blindé like a motherfucking tank

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -8,11 +8,15 @@
 			restrict: 'AEC',
 			link: function(scope, element, attrs) {
 				console.log('orsStar', arguments);
+				var note = Number(attrs.note) || 3;
+				note = (isNaN(note)) ? 3 : note;
+				note = note > 5 ? 5 : note;
+				note = note < 0 ? 0 : note;
 				var html = '';
-				for (var i = 0; i < 2; i++){
+				for (var i = 0; i < note; i++){
 					html += '<img src="ors-star/img/yellow_star.png" />';
 				}
-				for (var i = 0; i < 3; i++){
+				for (var i = note; i < 5; i++){
 					html += '<img src="ors-star/img/white_star.png" />';
 				}
 				element.append(html);
