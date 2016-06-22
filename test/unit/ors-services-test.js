c852ab5ed50331f1ec6d1a4c2ba58a46e4c6aca5
Maxime PIRES <maxime.pires@outlook.com>
2016-06-22 12:03:09 +0100

    Karma beach

---
+++ test/unit/ors-services-test.js
@@ -0,0 +1,13 @@
+(function() {
+	'use strict';
+
+	describe('ors-products', function() {
+
+		describe('coucou', function() {
+			it('should be fine', function() {
+				expect(0).toEqual(0);
+			});
+		});
+	});
+})();
+
